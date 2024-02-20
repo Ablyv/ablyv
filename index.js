df049de4cd8d69265f3d431baf643168c10f2c82
NebulousCalm <zjrichman@outlook.com>
2024-02-19 21:08:38 -0500

    commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+console.log("test console is active");
+
 app.use(express.urlencoded({ extended: true }));
 app.use('/static/', express.static("./static"));
 app.engine('html', ejs.renderFile);
@@ -18,9 +20,9 @@ app.get('/', function (req, res) {
 
 /* ---- LUMINA ROUTING ---- */
 app.get('/lumina', function (req, res){
-   res.render('lumina/landing.html', {
-       response: false
-   })
+    res.render('lumina/landing.html', {
+        response: false
+    })
 });
 
 app.listen(port, function () {
