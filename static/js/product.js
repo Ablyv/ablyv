f9f506709b70f0ba323618e8e39153e40fa50f3b
Nebulous <88895551+NebulousCalm@users.noreply.github.com>
2024-03-19 13:02:49 -0400

    NEW product page update

--- static/js/product.js
+++ static/js/product.js
@@ -1,74 +1,75 @@
 var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-    var that = this;
-    var delta = 150 - Math.random() * 100;
+  var that = this;
+  var delta = 150 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) { delta /= 2; }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
+  if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+  }
 
-    setTimeout(function() {
-        that.tick();
-    }, delta);
+  setTimeout(function() {
+      that.tick();
+  }, delta);
 };
 
 window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
 
-document.addEventListener('keydown', (event) =>{
-   if (event.keyCode === 191){
-       document.getElementById('topnav-search').focus();
-   }
-});
+// Above Code sourced from @Simon Shahriveri on codepen //
 
-const node = document.getElementById("topnav-search");
-node.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        const path = window.location + '../search';
-        const params = new URLSearchParams(window.location.search);
-        const hash = window.location.hash;
-        params.set('search', node.value);
-        window.history.replaceState({}, '', `${path}?${params.toString()}${hash}`);
+const updateStats = () =>{
+  const stats = {
+    osProj: 5,
+    satisfaction: 100,
+    revenue: 0.2,
+    reqs: 5
+  }
 
-    }
-});
+  let colorAll = document.getElementsByClassName('stats-text');
+
+  colorAll[0].innerHTML = stats.osProj + colorAll[0].innerHTML;
+  colorAll[1].innerHTML = stats.satisfaction + colorAll[1].innerHTML;
+  colorAll[2].innerHTML = stats.revenue + colorAll[2].innerHTML;
+  colorAll[3].innerHTML = stats.reqs + colorAll[3].innerHTML;
+}
+document.onload = updateStats();
